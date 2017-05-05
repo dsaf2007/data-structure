ff2840eb43f37aae02a6b6de6dc28a4eeda12399
CDG <dsaf2007@naver.com>
2017-05-05 14:37:22 +0900

    lab8 including commit

--- lab08/lab08/bstree.cpp
+++ lab08/lab08/bstree.cpp
@@ -1,13 +1,4 @@
-//--------------------------------------------------------------------
-//
-//  Laboratory 12                                         bstree.cpp
-//  Author: James Roberge lab manual prelab solution
-//
-//  SOLUTION: Linked implementation of the Binary Search Tree ADT
-//
-//--------------------------------------------------------------------
 
-#include <assert.h>
 
 #include "bstree.h"
 
@@ -18,8 +9,7 @@ BSTreeNode<TE,KF>:: BSTreeNode ( const TE &elem,
 				 BSTreeNode<TE,KF> *leftPtr,
 				 BSTreeNode<TE,KF> *rightPtr )
 
-// Creates a binary search tree node containing element elem, left
-// child pointer leftPtr, and right child pointer rightPtr.
+// 이진트리 생성.
 
   : element(elem),
     left(leftPtr),
@@ -32,7 +22,7 @@ BSTreeNode<TE,KF>:: BSTreeNode ( const TE &elem,
 template < class TE, class KF >
 BSTree<TE,KF>:: BSTree ()
 
-// Creates an empty tree.
+//비어있는 이진트리 생성
 
   : root(0)
 
@@ -43,7 +33,7 @@ BSTree<TE,KF>:: BSTree ()
 template < class TE, class KF >
 BSTree<TE,KF>:: ~BSTree ()
 
-// Frees the memory used by a tree.
+//메모리 해제
 
 {
     clear();
@@ -54,10 +44,8 @@ BSTree<TE,KF>:: ~BSTree ()
 template < class TE, class KF >
 void BSTree<TE,KF>:: insert ( const TE &newElement )
 
-// Inserts newElement into a tree. If an element with the same key
-// as newElement already exists in the tree, then updates that
-// element's data with newElement's data.
 
+//트리에 newElement삽입. 동일한 element가 tree에 존재할 경우 자료 업데이트
 {
     insertSub(root,newElement);
 }
@@ -68,19 +56,18 @@ template < class TE, class KF >
 void BSTree<TE,KF>:: insertSub ( BSTreeNode<TE,KF> *&p,
                                  const TE &newElement   )
 
-// Recursive partner of the insert() function. Inserts newElement in
-// the subtree pointed to by p.
 
+//insert의 재귀적함수. p에 newElement를 넣는다
 {
     if ( p == 0 )
-    {                                                  // Insert
+    {                                                  // 삽입
        p = new BSTreeNode<TE,KF>(newElement,0,0);
        assert ( p != 0 );
     }
     else if ( newElement.key() < p->element.key() )
-       insertSub(p->left,newElement);                  // Search left
+       insertSub(p->left,newElement);                  // 왼쪽 검색
     else if ( newElement.key() > p->element.key() )
-       insertSub(p->right,newElement);                 // Search right
+       insertSub(p->right,newElement);                 //오른쪽 검색
     else
        p->element = newElement;                        // Update
 }
@@ -90,10 +77,8 @@ void BSTree<TE,KF>:: insertSub ( BSTreeNode<TE,KF> *&p,
 template < class TE, class KF >
 int BSTree<TE,KF>:: retrieve ( KF searchKey, TE &searchElement ) const
 
-// Searches a tree for the element with key searchKey. If the element
-// is found, then copies the element to searchElement and returns 1.
-// Otherwise, returns 0 with searchElement undefined.
 
+//트리안에 element가 있는지 검색한다. 존재할경우 element를 copy하고 1을 return한다.
 {
     return retrieveSub(root,searchKey,searchElement);
 }
@@ -105,11 +90,10 @@ int BSTree<TE,KF>:: retrieveSub ( BSTreeNode<TE,KF> *p,
                                   KF searchKey,
                                   TE &searchElement     ) const
 
-// Recursive partner of the retrieve() function. Searches the subtree
-// pointed to by p.
+//retrieve함수의 재귀적 함수
 
 {
-    int result;   // Result returned
+    int result;  
 
     if ( p == 0 )
        result = 0;
@@ -134,7 +118,7 @@ int BSTree<TE,KF>:: remove ( KF deleteKey )
 // Removes the element with key deleteKey from a tree. If the
 // element is found, then deletes it from the tree and returns 1.
 // Otherwise, returns 0.
-
+//element가 tree에 존재할 경우 제거하고 1을 return한다.
 {
     return removeSub(root,deleteKey);
 }
@@ -145,36 +129,29 @@ template < class TE, class KF >
 int BSTree<TE,KF>:: removeSub ( BSTreeNode<TE,KF> *&p,
                                 KF deleteKey           )
 
-// Recursive partner of the remove() function. Searches the subtree
-// pointed to by p for the node with key deleteKey. If this node is
-// found, then does one of the following:
-//
-//    - If the node is missing one or more children, then links
-//      around it and deletes it.
-//
-//    - Otherwise, uses cutRightmost to replace the data in the
-//      node with the data in the rightmost descendant of the node's
-//      left child and deletes that node.
 
+//remove함수의 재귀적 함수
+//노드가 children을 하나라도 가지고 있지 않을 경우 그 노드를 삭제한다.
+//가지고 있는경우에는 cutRightmost를 이용해서 node를 대체하고 cutRightmost를 삭제한다.
 {
-    BSTreeNode<TE,KF> *delPtr;  // Pointer to node to delete
-    int result;                 // Result returned
+    BSTreeNode<TE,KF> *delPtr; 
+    int result;                 
 
     if ( p == 0 )
-       result = 0;                               // Search failed
+       result = 0;                              
     else if ( deleteKey < p->element.key() )
        result = removeSub(p->left,deleteKey);    // Search left
     else if ( deleteKey > p->element.key() )
        result = removeSub(p->right,deleteKey);   // Search right
     else
-    {                                            // Found
+    {                                      
        delPtr = p;
        if ( p->left == 0 )
-          p = p->right;                    // No left child
+          p = p->right;                    // left child없을경우
        else if ( p->right == 0 )
-          p = p->left;                     // No right child
+          p = p->left;                     // right child없을경우
        else
-          cutRightmost(p->left,delPtr);    // Has both children
+          cutRightmost(p->left,delPtr);    // 다있을경우
        delete delPtr;
        result = 1;
     }
@@ -188,10 +165,8 @@ template < class TE, class KF >
 void BSTree<TE,KF>:: cutRightmost ( BSTreeNode<TE,KF> *&r,
                                     BSTreeNode<TE,KF> *&delPtr )
 
-// Recursive partner of the removeSub() function. Traces down a
-// sequence of right children. Copies the data from the last node in
-// the sequence into the node pointed to delPtr, sets delPtr to point
-// to the last node in the sequence, and links around this node.
+
+//removeSub의 기능을 돕는 함수. 마지막 노드에 있는 data를 delPtr에 복사하고 delPtr가 마지막 노드를point하게 한다.
 
 {
     if ( r->right != 0 )
@@ -209,7 +184,8 @@ void BSTree<TE,KF>:: cutRightmost ( BSTreeNode<TE,KF> *&r,
 template < class TE, class KF >
 void BSTree<TE,KF>:: writeKeys () const
 
-// Outputs the keys in a tree in ascending order.
+// 오름차순으로 모든 key를 출력한다
+
 
 {
     writeKeysSub(root);
@@ -220,8 +196,7 @@ void BSTree<TE,KF>:: writeKeys () const
 template < class TE, class KF >
 void BSTree<TE,KF>:: writeKeysSub ( BSTreeNode<TE,KF> *p ) const
 
-// Recursive partner of the writeKeys() function. Outputs the keys in
-// the subtree pointed to by p.
+//writeKeys의 재귀적 함수
 
 {
     if ( p != 0 )
@@ -237,7 +212,7 @@ void BSTree<TE,KF>:: writeKeysSub ( BSTreeNode<TE,KF> *p ) const
 template < class TE, class KF >
 void BSTree<TE,KF>:: clear ()
 
-// Removes all the nodes from a tree.
+// 모든 노드를 지운다
 
 {
     clearSub(root);
@@ -249,9 +224,7 @@ void BSTree<TE,KF>:: clear ()
 template < class TE, class KF >
 void BSTree<TE,KF>:: clearSub ( BSTreeNode<TE,KF> *p )
 
-// Recursive partner of the clear() function. Clears the subtree
-// pointed to by p.
-
+// clear함수의 재귀적 함수
 {
     if ( p != 0 )
     {
@@ -266,25 +239,11 @@ void BSTree<TE,KF>:: clearSub ( BSTreeNode<TE,KF> *p )
 template < class TE, class KF >
 int BSTree<TE,KF>:: empty () const
 
-// Returns 1 if a tree is empty. Otherwise returns 0.
+// tree가 비었을 경우 1return
 
 {
     return ( root == 0 );
 }
-
-//--------------------------------------------------------------------
-
-template < class TE, class KF >
-int BSTree<TE,KF>:: full () const
-
-// Returns 1 if a tree is full. Otherwise returns 0. Cannot be done
-// cleanly in generic C++.
-
-{
-    return (  sizeof(TE) );
-}
-
-//--------------------------------------------------------------------
 
 template < class TE, class KF >
 void BSTree<TE,KF>:: showStructure () const
@@ -338,7 +297,7 @@ void BSTree<TE,KF>:: showSub ( BSTreeNode<TE,KF> *p,
 
 template < class TE, class KF >
 int BSTree<TE, KF>::height() const
-
+//이진트리의 깊이를 나타내는 함수
 {
 	
 	return heightSub(root);
@@ -347,7 +306,9 @@ int BSTree<TE, KF>::height() const
 
 template < class TE, class KF >
 int BSTree<TE, KF>::heightSub(BSTreeNode<TE, KF> *p) const
-
+//height의 재귀적함수
+//각 노드에서 left right가 NULL이 될때까지 이동하면서 왼쪽으로 이동할땐 왼쪽 깊이인 l에 +1
+//오른쪽으로 이동할땐 오른쪽 깊이인 r에 +1을 하여 둘중 큰 값을 반환
 {
 
 
